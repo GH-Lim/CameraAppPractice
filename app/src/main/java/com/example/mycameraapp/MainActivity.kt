42dcbbd45a6872c5ec1e5e21079e730e7aac7e7c
임건혁 <gunhyuck11@naver.com>
2020-04-06 23:35:49 +0900

    갤러리에서 이미지 가져오기

--- app/src/main/java/com/example/mycameraapp/MainActivity.kt
+++ app/src/main/java/com/example/mycameraapp/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.mycameraapp
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +28,10 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener { view ->
             dispatchTakePictureIntent()
+        }
+
+        button2.setOnClickListener { view ->
+            openGallery()
         }
     }
 
@@ -43,6 +50,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     val REQUEST_IMAGE_CAPTURE = 1
+    val OPEN_GALLERY = 2
 
     private fun dispatchTakePictureIntent() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
@@ -56,6 +64,21 @@ class MainActivity : AppCompatActivity() {
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             val imageBitmap = data?.extras?.get("data") as Bitmap
             imageView.setImageBitmap(imageBitmap)
+        } else if (requestCode == OPEN_GALLERY && resultCode == Activity.RESULT_OK) {
+            val currentImageUrl : Uri? = data?.data
+
+            try{
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
+                imageView.setImageBitmap(bitmap)
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
         }
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.setType("image/*")
+        startActivityForResult(intent, OPEN_GALLERY)
     }
 }
